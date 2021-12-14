96b53191f86458ef836ef197f61a4840c3a6e1ba
Sumedha Deshpande <sumedhasd1308@gmail.com>
2021-12-15 02:04:21 +0530

    First setup and homepage

---
+++ js/app.js
@@ -0,0 +1,28 @@
+{
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector("nav");
+  const homeSection = document.querySelector(".home-section");
+  let isHamburgerOpen = false;
+  hamburger.addEventListener("click", () => {
+    if (isHamburgerOpen === false) {
+      hamburger.classList.add("open");
+      nav.classList.add("nav-open");
+      homeSection.classList.add("home-section-responsive");
+      isHamburgerOpen = true;
+    } else {
+      hamburger.classList.remove("open");
+      nav.classList.remove("nav-open");
+      homeSection.classList.remove("home-section-responsive");
+      isHamburgerOpen = false;
+    }
+  });
+
+  nav.addEventListener("click", () => {
+    if (nav.className.includes("nav-open")) {
+      hamburger.classList.remove("open");
+      nav.classList.remove("nav-open");
+      homeSection.classList.remove("home-section-responsive");
+      isHamburgerOpen = false;
+    }
+  });
+}
