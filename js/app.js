e2c35122ead7dd60b28039167b82b8bc3c3f07b6
Sumedha Deshpande <sumedhasd1308@gmail.com>
2022-02-09 21:34:55 +0530

    Changing resume link

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
 {
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector("nav");
-  const homeSection = document.querySelector(".home-section");
-  let isHamburgerOpen = false;
-  hamburger.addEventListener("click", () => {
-    if (isHamburgerOpen === false) {
-      hamburger.classList.add("open");
-      nav.classList.add("nav-open");
-      homeSection.classList.add("home-section-responsive");
-      isHamburgerOpen = true;
-    } else {
-      hamburger.classList.remove("open");
-      nav.classList.remove("nav-open");
-      homeSection.classList.remove("home-section-responsive");
-      isHamburgerOpen = false;
-    }
-  });
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+    const homeSection = document.querySelector('.home-section');
+    let isHamburgerOpen = false;
+    hamburger.addEventListener('click', () => {
+        if (isHamburgerOpen === false) {
+            hamburger.classList.add('open');
+            nav.classList.add('nav-open');
+            homeSection.classList.add('home-section-responsive');
+            isHamburgerOpen = true;
+        } else {
+            hamburger.classList.remove('open');
+            nav.classList.remove('nav-open');
+            homeSection.classList.remove('home-section-responsive');
+            isHamburgerOpen = false;
+        }
+    });
 
-  nav.addEventListener("click", () => {
-    if (nav.className.includes("nav-open")) {
-      hamburger.classList.remove("open");
-      nav.classList.remove("nav-open");
-      homeSection.classList.remove("home-section-responsive");
-      isHamburgerOpen = false;
-    }
-  });
+    nav.addEventListener('click', () => {
+        if (nav.className.includes('nav-open')) {
+            hamburger.classList.remove('open');
+            nav.classList.remove('nav-open');
+            homeSection.classList.remove('home-section-responsive');
+            isHamburgerOpen = false;
+        }
+    });
+    // eslint-disable-next-line no-console
 }
